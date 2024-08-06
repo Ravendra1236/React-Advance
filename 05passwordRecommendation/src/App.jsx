9b8a2b7d621b514fda06f82be7bf1970ec635f4d
Ravendra1236 <ravendras189@gmail.com>
2024-08-06 19:18:36 +0530

    05passwordRecommendation

---
+++ 05passwordRecommendation/src/App.jsx
@@ -0,0 +1,100 @@
+import { useCallback, useEffect, useState , useRef } from 'react'
+import './App.css'
+
+function App() {
+  const [length , setLength] = useState(8);
+  const [numberAllowed , setNumberAllowed ] = useState(false);
+  const [specialChars, setSpecialChars] = useState(false);
+  const [password , setPassword] = useState("");
+
+  // Ref hook : 
+  const passwordRef = useRef(null)  // we used this to give good UX
+
+  // React me useCallback hook ko tab use kiya jata hai jab aapko ek function ko memoize karna hota hai, taki unnecessary re-renders ko roka ja sake. Yeh tab kaam aata hai jab aap ek function ko pass kar rahe hote hain as a prop to child components aur aap nahi chahte ki woh function har render par recreate ho.
+
+//   const memoizedCallback = useCallback(() => {
+//   // your logic here
+// }, [dependencies]);
+  
+const passwordGenerator = useCallback(()=>{
+  let pass = "";
+  let str = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
+
+  if(numberAllowed) str += "0123456789";
+  if(specialChars) str += "!@#$%^&*()_+=-{}[]|:;<>,.?/~";
+
+  for(let i = 1 ; i<=length ; i++){
+    let char = Math.floor(Math.random() * str.length + 1);
+    pass += str[char];
+  }
+
+  setPassword(pass);
+  
+} , [length , specialChars , numberAllowed , setPassword]);
+
+// Copy Button using ref Hook : 
+const copyToClipboard = useCallback(()=>{
+  passwordRef.current?.select();
+  // passwordRef.current?.setSelectionRange(0,20);
+  window.navigator.clipboard.writeText(password)
+}, [password])
+
+// setPassword is used only for memoization : Sotred in cache 
+// We can also remove it
+
+// useEffect hook is used to run side effects after render, it is a good place to put API calls, subscriptions, or other side effects that affect the component's render output.
+useEffect(()=>{
+  passwordGenerator();
+}, [length , specialChars , numberAllowed , passwordGenerator]);
+
+  return (
+    <div className='w-full max-w-md mx-auto shadow-md rounded-lg px-4 py-3 my-8  bg-gray-700 text-orange-500'>
+      <h1 className='text-white text-center'>Password Generator</h1>
+      <div className='flex shadow rouned-lg overflow-hidden mb-4'>
+        <input type="text"
+        value={password} 
+        placeholder='Password'
+        className='outline-none w-full py-1 px-3'
+        readOnly
+        // Copy on clipboard 
+        ref={passwordRef}
+        />
+        <button onClick={copyToClipboard} 
+        className='outline-none bg-blue-700 text-white px-3 py-0.5 shrink-0'>Copy</button>
+      </div>
+      <div className='flex text-sm gap-x-2'>
+        <div className='flex items-center gap-x-1'>
+        <input type="range"
+        min={5}
+        max={100}
+        value={length}
+        className='cursor-pointer'
+        // This is for changing range value by curson its an event handler
+        onChange={(e)=>{setLength(e.target.value);}}
+        />
+        <label>Length: {length}</label>
+        </div>
+
+        <div className='flex items-center gap-x-1'>
+          <input type="checkbox"
+          defaultChecked = {numberAllowed}
+          id='numberInput'
+          onChange={()=>{setNumberAllowed((prev) => !prev)}}
+          />
+          <label>Numbers</label>
+        </div>
+
+        <div className='flex items-center gap-x-1'>
+          <input type="checkbox"
+          defaultChecked = {numberAllowed}
+          id='characterInput'
+          onChange={()=>{setSpecialChars((prev) => !prev)}}
+          />
+          <label>Special Characters</label>
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default App
